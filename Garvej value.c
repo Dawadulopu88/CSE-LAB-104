7d93b2713ec3b9a2b074f375eb2e76d7f82b53da
Md. Daudul Islam Opu <140642063+Dawadulopu88@users.noreply.github.com>
2023-08-03 10:38:16 +0600

    Add files via upload

---
+++ Garvej value.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main (){
+int i;
+for(;;){
+    printf("Hello\n");
+    i++;
+    if(i>=10){
+        printf("Ending value of i=%d\n",i);
+       break;
+       }
+
+ }
+
+return 0;
+}
+
